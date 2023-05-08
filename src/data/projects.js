68e7f81fbf7196832f362ae71203df3e74521129
nahidnstu12 <nahid.dev19@gmail.com>
2023-05-08 08:39:08 +0600

    project data add 2

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,8 @@ export const office_projects = [
   {
     id: 1,
     title: "Future Nation Web",
+    development: "Softbdltd",
+    active: true,
     description: [
       "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy. ",
       "The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions",
@@ -26,13 +28,15 @@ export const office_projects = [
   {
     id: 2,
     title: "NISE3",
+    development: "Softbdltd",
+    active: true,
     description: [
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy. ",
-      "The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions",
-      "",
+      "NISE3 stands National Intelligence for Skills, Education, Employment and Entrepreneurship.Real-time data driven Matchmaking Platform for Skills, Education, Employment & Entrepreneurship.",
+      "This is a one-stop platform where the stakeholders can fulfill the needs of one another for stimulating economic growth. NISE is simplifying and digitalizing the services offered by public & private skills service providers as well as sharing real-time data for job forecasting and market analysis to transform their mechanism from supply driven to market demand. Policy Makers can monitor and evaluate the youth database, entrepreneur database, access employment tracker feedback and other real-time data in an analytical framework that ultimately helps in evidence-based decision making.",
+      "The registered employable youths are able to access information on AI based career guidance, skills trainings, job forecast, apprenticeship opportunities, business and entrepreneurship guidance, migration suggestions, etc.",
     ],
     short_description:
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy.The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions.",
+      "NISE3 stands National Intelligence for Skills, Education, Employment and Entrepreneurship.Real-time data driven Matchmaking Platform for Skills, Education, Employment & Entrepreneurship. This is a one-stop platform where stakeholders can fulfill their needs.",
     techstack: [
       "NextJS",
       "Laravel",
@@ -47,6 +51,32 @@ export const office_projects = [
     snapshots: [""],
     liveUrl: ["https://futurenation.gov.bd/"],
   },
+  {
+    id: 3,
+    title: "E-toss",
+    development: "PromoteUP",
+    active: true,
+    description: [
+      "The clothing brand is an online marketplace that operates on a multivendor system, allowing different vendors to sell their products through the platform. The platform caters to both wholesale and reseller customers, with prices varying based on the type of customer.",
+      "This system to provide end-to-end platform for small businesses to manage their operations online.This offers selling products, an inventory management system, a point-of-sale system, digital payments, delivery system and logistics services for order fulfillment. ",
+      "Overall, this platform aims to provide a convenient and user-friendly platform for buying and selling custom clothing products, while enabling vendors to maintain control over their own businesses hassle free and zero investment.",
+    ],
+    short_description:
+      "The clothing brand is an online marketplace that allows vendors to sell their products on a multivendor system to wholesale and reseller customers. The platform also provides an end-to-end platform for small businesses with features such as selling products, inventory management, point-of-sale, digital payments, delivery system, and logistics services. ",
+    techstack: [
+      "NestJS",
+      "NextJS",
+      "Node",
+      "MySQL",
+      "MongoDB",
+      "Keyclock",
+      "RabbitMQ",
+      "AI",
+      "Go",
+    ],
+    snapshots: [""],
+    liveUrl: ["https://futurenation.gov.bd/"],
+  },
 ];
 
 export const personal_projects = [
@@ -55,12 +85,12 @@ export const personal_projects = [
     title: "Bookmark",
     active: true,
     description: [
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy. ",
-      "The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions",
-      "",
+      "Bookmark is an e-commerce website that allows the admin to manage various publications and books from different authors, with variant types. Users can add their desired books to their cart, mark them as favorites, and leave reviews on the website. The website supports payment through Stripe and cash on delivery.",
+      " This was a team project with six members, developed as part of an assignment in a Full-Stack Army boot camp. The project was acknowledged as the best project by the instructor.",
+      "Overall, Bookmark is a comprehensive e-commerce platform that provides a user-friendly experience for both the admin and the customers, allowing for easy management of book data and a seamless buying experience for customers.",
     ],
     short_description:
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy.The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions.",
+      "Bookmark is an e-commerce platform that allows admin to manage publications and books from different authors. Users can add books to their cart, mark as favorites, and leave reviews. Payments can be made through Stripe or cash on delivery. The platform was developed by a team of six members during a Full-Stack Army boot camp, and was acknowledged as the best project by the instructor. ",
     techstack: [
       "NextJS",
       "Strapi",
@@ -79,13 +109,13 @@ export const personal_projects = [
     title: "Clean Youtube",
     active: true,
     description: [
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy. ",
-      "The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions",
-      "",
+      "Clean YouTube is a platform designed for users who want to learn from YouTube without any distractions and minimal design. This app allows users to watch playlists without being interrupted by suggested videos or other distractions. Clean YouTube has four main sections: Home, Playlists, Favorite, and Recent.",
+      "With its four main sections and single playlist player, users can easily find and watch the playlists they like. Additionally, the app's note-taking feature allows users to keep track of their progress and remember important information.",
+      "In single player, user can remove sidebar, description, and notes sections.This allows the user to more concentration when watching tutorials.",
     ],
     short_description:
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy.The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions.",
-    techstack: ["NextJS", "Youtube api v3", "Redux", "rtk-query", "typescript"],
+      "Clean YouTube is a platform that helps users learn from YouTube without distractions. The app has four main sections: Home, Playlists, Favorite, and Recent. Users can watch playlists without interruptions and take notes to keep track of their progress. The single player with dynamic layout control feature allows users to remove distractions and concentrate better while watching tutorials.",
+    techstack: ["NextJS", "Youtube api v3", "Redux", "rtk-query", "typescript", "react-player", "MUI"],
     snapshots: [""],
     liveUrl: ["https://clean-youtube-playlists.vercel.app/"],
     sourceUrl: ["https://github.com/nahidnstu12/clean-youtube"],
@@ -94,12 +124,13 @@ export const personal_projects = [
     id: 3,
     title: "CollageLMS",
     description: [
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy. ",
-      "The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions",
-      "",
+      "CollageLMS is a college management system that managing various college-related activities. The system is designed to cater to the needs of three main types of users: students, teachers, and super admin.",
+      "Super Admin can create and manage teachers, admit students, create and manage subjects, classes, and routines. The super admin can also set up the grading system, manage attendance records, and generate reports based on the data collected.",
+      "Teachers can manage their classes, including scheduling exams, managing results, and taking attendance.",
+      "Students can view their results, attendance records, and class notes. The system allows students to access their academic records, track their progress, and communicate with their teachers."
     ],
     short_description:
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy.The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions.",
+      "CollageLMS is a college management system with three main user types: students, teachers, and super admin. The super admin can manage teachers, students, subjects, classes, routines, grading, attendance, and generate reports. Teachers can manage classes, exams, results, and attendance, while students can access their academic records, view results, attendance, and class notes.",
     techstack: [
       "NextJS",
       "Laravel",
@@ -115,12 +146,12 @@ export const personal_projects = [
     id: 4,
     title: "JapanGardenShop",
     description: [
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy. ",
-      "The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions",
-      "",
+      "JapanGardenShop is a small clothing shop that mainly lists women's and baby dresses. Users can add the desired items to their cart or mark them as favorites for later purchase. ",
+      "Customers can order in two ways: either through delivery or by booking the dress and buying it directly from the shop. Payment methods include cash on delivery and bKash.",
+      "Overall, JapanGardenShop aims to provide a seamless buying experience for customers who are interested in purchasing unique and stylish clothing items.",
     ],
     short_description:
-      "Futurenation is a coalition of government, private sector, and development leaders working together to transform the country's population dividend into a driving force for the economy.The website includes an Assessment Portal for customized career tests, a Learning Management System for skill-enhancing courses recommended by AI, and a Job Recommendation Engine for AI-based job suggestions.",
+      "JapanGardenShop is a small clothing shop that sells women's and baby dresses. Customers can order items through delivery or by booking and buying them directly from the shop. Payment methods include cash on delivery and bKash. The platform provides a user-friendly interface for managing order processes.",
     techstack: [
       "NextJS",
       "Laravel",
